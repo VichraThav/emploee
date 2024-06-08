6cd4975f42edd5e3a4bf3a2d7bb6ba2d105d8efa
thises-group <thavcichra@gmail.com>
2024-06-08 09:34:10 +0700

    test-1

---
+++ testing/resources/views/test.blade.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Employee</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="{{url('css/style.css')}}">
+</head>
+<body>
+    <div class="container-fluid pt-5 pb-5 bg-danger" style="border:1px solid red; height:100vh;">
+        <div class="container border h-100 bg-white rounded-4" style="width: 600px;">
+            <h1 class="w-100 text-center fw-bold mt-4">Employee Form</h1>
+            <form action="">
+                <div class="mb-3">
+                    <label for="exampleInputEmail1" class="form-label m-auto" >Email address</label>
+                    <input type="text" class="form-control m-auto" id="exampleInputEmail1" aria-describedby="emailHelp" >
+                    
+                  </div>
+                  <div class="mb-3">
+                    <label for="exampleInputPassword1" class="form-label">Password</label>
+                    <input type="password" class="form-control" id="exampleInputPassword1" >
+                  </div>
+                  <div class="mb-3">
+                    <label for="exampleInputEmail1" class="form-label m-auto" >Email address</label>
+                    <input type="text" class="form-control m-auto" id="exampleInputEmail1" aria-describedby="emailHelp" >
+                    
+                  </div>
+                  <div class="mb-3">
+                    <label for="exampleInputPassword1" class="form-label">Password</label>
+                    <input type="password" class="form-control" id="exampleInputPassword1" >
+                  </div>
+                  <div class="mb-3">
+                    <label for="exampleInputEmail1" class="form-label m-auto" >Email address</label>
+                    <input type="text" class="form-control m-auto" id="exampleInputEmail1" aria-describedby="emailHelp" >
+                    
+                  </div>
+                  <div class="mb-3">
+                    <label for="exampleInputPassword1" class="form-label">Password</label>
+                    <input type="password" class="form-control" id="exampleInputPassword1" >
+                  </div>
+                  <div class="container w-100 d-flex justify-content-center">
+                    <button type="submit" class="btn btn-primary mt-4" style="witdth:400px;">Submit</button>
+                  </div>
+            </form>
+        </div>
+    </div>
+</body>
+</html>
